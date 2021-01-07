2a9cc7376c55367195fb0d4cf40b3ef3ca83aa16
Ronak Mistry <59952103+ronak007mistry@users.noreply.github.com>
2021-01-08 00:36:28 +0530

    Few changes

--- recommendation_app/views.py
+++ recommendation_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import UserLocation, UserPreference, Users
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'recommendation_app/index.html')
+    users = Users.objects.all()
+
+    # username = request.GET['username']
+    # print(username)
+    context = {"users": users}
+    return render(request, 'recommendation_app/index.html', context=context)
     
